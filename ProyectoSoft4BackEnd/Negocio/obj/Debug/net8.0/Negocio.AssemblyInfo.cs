18216dfde999d8a0c24d629d71348b1aed65f4ce
uligi <59987716+uligi@users.noreply.github.com>
2024-11-18 17:40:06 -0600

    d

--- ProyectoSoft4BackEnd/Negocio/obj/Debug/net8.0/Negocio.AssemblyInfo.cs
+++ ProyectoSoft4BackEnd/Negocio/obj/Debug/net8.0/Negocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Negocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c7e24353707cde626a79d6d4c9335248c8bdd99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e56dc52b9fa817a3255936ae4d6830a884f36513")]
 [assembly: System.Reflection.AssemblyProductAttribute("Negocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Negocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
